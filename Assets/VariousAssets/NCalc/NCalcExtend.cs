13a4cb68a4bb1b5f136c9e39062dbcea748ad6fe
Ethan <yishionwang314@163.com>
2016-07-07 15:27:02 +0800

    add assets: LOOM / protobuf-net / NCalc

---
+++ Assets/VariousAssets/NCalc/NCalcExtend.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NCalc
+{
+	public static class NCalcExtend
+	{
+		public static Expression NCalc (this JToken jt)
+		{
+			string exp_str = jt.Value<string> ();
+			exp_str = exp_str.Replace ("TRUE", "true");
+			exp_str = exp_str.Replace ("True", "true");
+			exp_str = exp_str.Replace ("FALSE", "false");
+			exp_str = exp_str.Replace ("False", "false");
+			return new Expression (exp_str);
+		}
+	}
+}
