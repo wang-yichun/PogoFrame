c6c07d733d830d0fecf093d595dfc5f0eefca512
Ethan <yishionwang314@163.com>
2016-07-09 18:27:00 +0800

    ShareSDK Editor

---
+++ Assets/VariousSDK/ShareSDK/Editor/ShareSDKPostProcess.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Collections;
+using System.Diagnostics;
+using System.Xml;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using Debug = UnityEngine.Debug;
+
+public class ShareSDKPostProcess
+{
+
+	[PostProcessBuild (100)]
+	public static void OnPostprocessBuild (BuildTarget target, string pathToBuiltProject)
+	{
+		Debug.Log ("ShareSDKPostProcess began to run!!");
+	}
+
+	[MenuItem ("PogoTools/Run ShareSDK PostProcess #%c")]
+	public static void Execute ()
+	{
+		string pathToBuiltProject = @"/Users/EthanW/Documents/UnityProjects/PogoFrame/Builds/IOSProj/IOSProj";
+		OnPostprocessBuild (BuildTarget.iOS, pathToBuiltProject);
+	}
+}
