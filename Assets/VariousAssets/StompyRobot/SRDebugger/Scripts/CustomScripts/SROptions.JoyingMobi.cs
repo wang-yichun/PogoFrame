785ab1f074594aa9d5731a21d72a522db35bba2f
Ethan <yishionwang314@163.com>
2016-07-12 11:39:44 +0800

    Joying - SRD

---
+++ Assets/VariousAssets/StompyRobot/SRDebugger/Scripts/CustomScripts/SROptions.JoyingMobi.cs
@@ -0,0 +1,17 @@
+﻿//#define SDK_JOYINGMOBI
+
+#if SDK_JOYINGMOBI
+using UnityEngine;
+using System.ComponentModel;
+using UnityEngine.Advertisements;
+using pogorock.Joying;
+
+public partial class SROptions
+{
+	[Category ("JoyingMobi"), DisplayName ("InitAppID")] 
+	public void Joying_InitAppID ()
+	{
+		Joying_Utility.Instance.InitAppID ("3ca89e8b4b868b6f", "9b95a47c62a3b4b1");
+	}
+}
+#endif
---
+++ Assets/VariousSDK/JoyingSDK/Extensions/IOSNative/Joying_Define.cs
@@ -0,0 +1,1 @@
+﻿#define SDK_JOYINGMOBI
