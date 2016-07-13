ea4889c7985ef2ec660ccc16ea10b41025aa6bb1
Ethan <yishionwang314@163.com>
2016-07-13 10:44:50 +0800

    DetachableAssetsManager 初步

---
+++ Assets/VariousAssets/DetachableAssetsManager/Editor/DetachableAssetInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace pogorock
+{
+	using UnityEngine;
+	using System.Collections;
+	using UnityEditor;
+
+	public class DetachableAssetInfo
+	{
+		public string Name;
+		public string DevDataPathRoot;
+		public string AssetsPathRoot;
+		public string Symbol;
+	}
+}
---
+++ Assets/VariousAssets/DetachableAssetsManager/Editor/DetachableAssetsManager.cs
@@ -0,0 +1,66 @@
+﻿namespace pogorock
+{
+	using UnityEngine;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEditor;
+	using Newtonsoft.Json;
+	using System.IO;
+
+	public class DetachableAssetsManagerWindow : EditorWindow
+	{
+		[MenuItem ("PogoTools/Detachable Assets Manager #%d")]
+		static void AddWindow ()
+		{
+			//创建窗口
+			Rect rect = new Rect (0, 0, 500, 500);
+			DetachableAssetsManagerWindow window = (DetachableAssetsManagerWindow)EditorWindow.GetWindowWithRect (
+				                                       typeof(DetachableAssetsManagerWindow),
+				                                       rect,
+				                                       true,
+				                                       "Detachable Assets Manager(可拆卸资源管理器)"
+			                                       );	
+			window.Show ();
+		}
+
+		void Awake ()
+		{
+			tryCreateASampleConfig ();
+			loadConfig ();
+		}
+
+		void OnGUI ()
+		{
+			Debug.Log ("OnGUI()");
+		}
+
+		List<DetachableAssetInfo> ConfigList;
+
+		private static readonly string ConfigFilePath = "Assets/VariousAssets/DetachableAssetsManager/Editor";
+		private static readonly string ConfigFileName = "DetachableAssetsManagerConfig.txt";
+
+		private static string ConfigFileFullPath {
+			get {
+				return Path.Combine (ConfigFilePath, ConfigFileName);
+			}
+		}
+
+		void tryCreateASampleConfig ()
+		{
+			if (File.Exists (ConfigFileFullPath) == false) {
+				ConfigList = new List<DetachableAssetInfo> ();
+				ConfigList.Add (new DetachableAssetInfo ());
+				using (StreamWriter sw = File.CreateText (ConfigFileFullPath)) {
+					string text = JsonConvert.SerializeObject (ConfigList, Formatting.Indented);
+					sw.Write (text);
+				}
+			}
+		}
+
+		void loadConfig ()
+		{
+			string text = File.ReadAllText (ConfigFileFullPath);
+			ConfigList = JsonConvert.DeserializeObject<List<DetachableAssetInfo>> (text);
+		}
+	}
+}
