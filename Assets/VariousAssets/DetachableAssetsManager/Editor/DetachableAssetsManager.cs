fe1ba8f6d3f4fb8446ced679466423d15c8191bb
Ethan <yishionwang314@163.com>
2016-07-13 13:38:12 +0800

    filesCopy test

--- Assets/VariousAssets/DetachableAssetsManager/Editor/DetachableAssetsManager.cs
+++ Assets/VariousAssets/DetachableAssetsManager/Editor/DetachableAssetsManager.cs
@@ -7,7 +7,7 @@
 	using Newtonsoft.Json;
 	using System.IO;
 
-	public class DetachableAssetsManagerWindow : EditorWindow
+	public partial class DetachableAssetsManagerWindow : EditorWindow
 	{
 		[MenuItem ("PogoTools/Detachable Assets Manager #%d")]
 		static void AddWindow ()
@@ -23,16 +23,32 @@
 			window.Show ();
 		}
 
+		Texture2D gizmo_opened;
+		Texture2D gizmo_closed;
+
 		void Awake ()
 		{
+			gizmo_opened = AssetDatabase.LoadAssetAtPath<Texture2D> (GetSysRootPath () + "Gizmos/gizmo_opened.psd");
+			gizmo_closed = AssetDatabase.LoadAssetAtPath<Texture2D> (GetSysRootPath () + "Gizmos/gizmo_closed.psd");
+			
 			tryCreateASampleConfig ();
 			loadConfig ();
 		}
 
+		public static string GetSysRootPath ()
+		{
+			return "Assets/VariousAssets/DetachableAssetsManager/";
+		}
+
 		void OnGUI ()
 		{
-			Debug.Log ("OnGUI()");
+			for (int i = 0; i < ConfigList.Count; i++) {
+				var info = ConfigList [i];
+				InfoItemLayout (info);
+			}
 		}
+
+		#region config file
 
 		List<DetachableAssetInfo> ConfigList;
 
@@ -61,6 +77,19 @@
 		{
 			string text = File.ReadAllText (ConfigFileFullPath);
 			ConfigList = JsonConvert.DeserializeObject<List<DetachableAssetInfo>> (text);
+		}
+
+		#endregion
+
+		void DoDetach (DetachableAssetInfo info)
+		{
+			Debug.Log ("Detach " + info.Name + ".");
+		}
+
+		void DoIntegrate (DetachableAssetInfo info)
+		{
+			Debug.Log ("Integrate " + info.Name + ".");
+			DAM_FilesCopy.copyDirectory (info.DevDataPathRoot, info.AssetsPathRoot);
 		}
 	}
 }
---
+++ Assets/VariousAssets/DetachableAssetsManager/Editor/DetachableAssetsManager_ItemLayout.cs
@@ -0,0 +1,56 @@
+﻿namespace pogorock
+{
+	using UnityEngine;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEditor;
+	using Newtonsoft.Json;
+	using System.IO;
+
+	public partial class DetachableAssetsManagerWindow : EditorWindow
+	{
+
+		void InfoItemLayout (DetachableAssetInfo info)
+		{
+			EditorGUILayout.BeginHorizontal ("box");
+			bool enable = false;
+			if (SymbolHelper.ExistSymbol (info.Symbol)) {
+				enable = true;
+			}
+
+			EditorGUILayout.BeginVertical ();
+			EditorGUILayout.Space ();
+			if (enable) {
+				GUILayout.Label (gizmo_opened, GUILayout.Width (80f), GUILayout.Height (30f));
+			} else {
+				GUILayout.Label (gizmo_closed, GUILayout.Width (80f), GUILayout.Height (30f));
+
+			}
+			EditorGUILayout.BeginHorizontal ();
+			if (GUILayout.Button ("集成")) {
+				DoIntegrate (info);
+			}
+			if (GUILayout.Button ("拆卸")) {
+				DoDetach (info);
+			}
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.EndVertical ();
+
+			fixedInfoContent (info);
+
+			EditorGUILayout.EndHorizontal ();
+		}
+
+		void fixedInfoContent (DetachableAssetInfo info)
+		{
+			EditorGUILayout.BeginVertical ();
+			GUILayout.Label (info.Name, EditorStyles.boldLabel);
+			GUILayout.Label ("    原 Assets 存放位置: " + info.DevDataPathRoot);
+			GUILayout.Label ("    项目中位置: " + info.AssetsPathRoot);
+			GUILayout.Label ("    预定义 Symbol: " + info.Symbol);
+			EditorGUILayout.Space ();
+			EditorGUILayout.EndVertical ();
+		}
+	}
+}
