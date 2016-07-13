c6f8608ac8f1483e2a22f4444d9d4de6990f60d2
Ethan <yishionwang314@163.com>
2016-07-13 11:16:34 +0800

    symbolHelper

---
+++ Assets/VariousAssets/DetachableAssetsManager/Editor/SymbolHelper.cs
@@ -0,0 +1,60 @@
+﻿namespace pogorock
+{
+	using UnityEngine;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEditor;
+	using System.Linq;
+
+	public static class SymbolHelper
+	{
+		public static List<BuildTargetGroup> targetGroup = new List<BuildTargetGroup> () {
+			BuildTargetGroup.Standalone,
+			BuildTargetGroup.iOS,
+			BuildTargetGroup.Android
+		};
+
+		// add new symbol
+		public static void AddNewSymbol (string symbol)
+		{
+			string new_symbol = symbol;
+			foreach (BuildTargetGroup buildTarget in targetGroup) {
+				string symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup (buildTarget);
+				string[] symbols_list = symbols.Split (new char[]{ ';' });
+				if (symbols_list.Contains (new_symbol) == false) {
+					symbols += ";" + new_symbol;
+				}
+				PlayerSettings.SetScriptingDefineSymbolsForGroup (buildTarget, symbols); 
+			}
+		}
+
+		public static bool ExistSymbol (string symbol)
+		{
+			int count = 0;
+			foreach (BuildTargetGroup buildTarget in targetGroup) {
+				string symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup (buildTarget);
+				string[] symbols_list = symbols.Split (new char[]{ ';' });
+
+				if (symbols_list.Contains (symbol)) {
+					count++;
+				}
+			}
+
+			if (count == targetGroup.Count) {
+				return true;
+			}
+			return false;
+		}
+
+		public static void DeleteSymbol (string symbol)
+		{
+			foreach (BuildTargetGroup buildTarget in targetGroup) {
+				string symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup (buildTarget);
+				string[] symbols_list = symbols.Split (new char[]{ ';' });
+				symbols_list = symbols_list.Where (_ => _ != symbol).ToArray ();
+				symbols = string.Join (";", symbols_list);
+				PlayerSettings.SetScriptingDefineSymbolsForGroup (buildTarget, symbols); 
+			}
+		}
+	}
+}
