01d4ea673e43291d9304be79b2af4f58641140d1
Ethan <yishionwang314@163.com>
2016-07-18 17:45:49 +0800

    删除 native

--- Assets/Extensions/AndroidManifestManager/Core/AMMSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class AMMSettings  {
-
-
-	public const string VERSION_NUMBER = "1.7";
-}
--- Assets/Extensions/StansAssetsCommon/Utilities/Extensions/SA_Gameobject.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public static class SA_Gameobject  {
-
-	public static Sprite ToSprite(this Texture2D texture) {
-		return Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f)); 
-	}
-}
