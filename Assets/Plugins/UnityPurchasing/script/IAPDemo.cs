71117acfc0803efc0ff9ad0e7947e912089c4e2a
Ethan <yishionwang314@163.com>
2016-08-30 14:51:56 +0800

    DETACH: ZipX

--- Assets/Plugins/UnityPurchasing/script/IAPDemo.cs
+++ Assets/Plugins/UnityPurchasing/script/IAPDemo.cs
@@ -11,6 +11,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Purchasing;
 using UnityEngine.UI;
+
 #if RECEIPT_VALIDATION
 using UnityEngine.Purchasing.Security;
 #endif
@@ -20,18 +21,20 @@ using UnityEngine.Purchasing.Security;
 /// To use with your account, configure the product ids (AddProduct)
 /// and Google Play key (SetPublicKey).
 /// </summary>
-[AddComponentMenu("Unity IAP/Demo")]
+[AddComponentMenu ("Unity IAP/Demo")]
 public class IAPDemo : MonoBehaviour, IStoreListener
 {
-	// Unity IAP objects 
+	// Unity IAP objects
 	private IStoreController m_Controller;
 	private IAppleExtensions m_AppleExtensions;
 	private ISamsungAppsExtensions m_SamsungExtensions;
 
-	private int m_SelectedItemIndex = -1; // -1 == no product
+	private int m_SelectedItemIndex = -1;
+	// -1 == no product
 	private bool m_PurchaseInProgress;
 
-	private Selectable m_InteractableSelectable; // Optimization used for UI state management
+	private Selectable m_InteractableSelectable;
+	// Optimization used for UI state management
 	private bool m_IsSamsungAppsStoreSelected;
 
 	#if RECEIPT_VALIDATION
@@ -41,30 +44,27 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 	/// <summary>
 	/// This will be called when Unity IAP has finished initialising.
 	/// </summary>
-	public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+	public void OnInitialized (IStoreController controller, IExtensionProvider extensions)
 	{
 		m_Controller = controller;
 		m_AppleExtensions = extensions.GetExtension<IAppleExtensions> ();
 		m_SamsungExtensions = extensions.GetExtension<ISamsungAppsExtensions> ();
 
-		InitUI(controller.products.all);
+		InitUI (controller.products.all);
 
 		// On Apple platforms we need to handle deferred purchases caused by Apple's Ask to Buy feature.
 		// On non-Apple platforms this will have no effect; OnDeferred will never be called.
-		m_AppleExtensions.RegisterPurchaseDeferredListener(OnDeferred);
+		m_AppleExtensions.RegisterPurchaseDeferredListener (OnDeferred);
 
-		Debug.Log("Available items:");
-		foreach (var item in controller.products.all)
-		{
-			if (item.availableToPurchase)
-			{
-				Debug.Log(string.Join(" - ",
-					new[]
-					{
+		Debug.Log ("Available items:");
+		foreach (var item in controller.products.all) {
+			if (item.availableToPurchase) {
+				Debug.Log (string.Join (" - ",
+					new[] {
 						item.metadata.localizedTitle,
 						item.metadata.localizedDescription,
 						item.metadata.isoCurrencyCode,
-						item.metadata.localizedPrice.ToString(),
+						item.metadata.localizedPrice.ToString (),
 						item.metadata.localizedPriceString,
 						item.transactionID,
 						item.receipt
@@ -73,40 +73,38 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 		}
 
 		// Prepare model for purchasing
-		if (m_Controller.products.all.Length > 0) 
-		{
+		if (m_Controller.products.all.Length > 0) {
 			m_SelectedItemIndex = 0;
 		}
 
 		// Populate the product menu now that we have Products
-		for (int t = 0; t < m_Controller.products.all.Length; t++)
-		{
-			var item = m_Controller.products.all[t];
-			var description = string.Format("{0} - {1}", item.metadata.localizedTitle, item.metadata.localizedPriceString);
+		for (int t = 0; t < m_Controller.products.all.Length; t++) {
+			var item = m_Controller.products.all [t];
+			var description = string.Format ("{0} - {1}", item.metadata.localizedTitle, item.metadata.localizedPriceString);
 
 			// NOTE: my options list is created in InitUI
-			GetDropdown().options[t] = new Dropdown.OptionData(description);
+			GetDropdown ().options [t] = new Dropdown.OptionData (description);
 		}
 
 		// Ensure I render the selected list element
-		GetDropdown().RefreshShownValue();
+		GetDropdown ().RefreshShownValue ();
 
 		// Now that I have real products, begin showing product purchase history
-		UpdateHistoryUI();
+		UpdateHistoryUI ();
 	}
 
 	/// <summary>
 	/// This will be called when a purchase completes.
 	/// </summary>
-	public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+	public PurchaseProcessingResult ProcessPurchase (PurchaseEventArgs e)
 	{
-		Debug.Log("Purchase OK: " + e.purchasedProduct.definition.id);
-		Debug.Log("Receipt: " + e.purchasedProduct.receipt);
+		Debug.Log ("Purchase OK: " + e.purchasedProduct.definition.id);
+		Debug.Log ("Receipt: " + e.purchasedProduct.receipt);
 
 		m_PurchaseInProgress = false;
 
 		// Now that my purchase history has changed, update its UI
-		UpdateHistoryUI();
+		UpdateHistoryUI ();
 
 		#if RECEIPT_VALIDATION
 		if (Application.platform == RuntimePlatform.Android ||
@@ -150,46 +148,45 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 	/// <summary>
 	/// This will be called is an attempted purchase fails.
 	/// </summary>
-	public void OnPurchaseFailed(Product item, PurchaseFailureReason r)
+	public void OnPurchaseFailed (Product item, PurchaseFailureReason r)
 	{
-		Debug.Log("Purchase failed: " + item.definition.id);
-		Debug.Log(r);
+		Debug.Log ("Purchase failed: " + item.definition.id);
+		Debug.Log (r);
 
 		m_PurchaseInProgress = false;
 	}
 
-	public void OnInitializeFailed(InitializationFailureReason error)
+	public void OnInitializeFailed (InitializationFailureReason error)
 	{
-		Debug.Log("Billing failed to initialize!");
-		switch (error)
-		{
-			case InitializationFailureReason.AppNotKnown:
-				Debug.LogError("Is your App correctly uploaded on the relevant publisher console?");
-				break;
-			case InitializationFailureReason.PurchasingUnavailable:
+		Debug.Log ("Billing failed to initialize!");
+		switch (error) {
+		case InitializationFailureReason.AppNotKnown:
+			Debug.LogError ("Is your App correctly uploaded on the relevant publisher console?");
+			break;
+		case InitializationFailureReason.PurchasingUnavailable:
 				// Ask the user if billing is disabled in device settings.
-				Debug.Log("Billing disabled!");
-				break;
-			case InitializationFailureReason.NoProductsAvailable:
+			Debug.Log ("Billing disabled!");
+			break;
+		case InitializationFailureReason.NoProductsAvailable:
 				// Developer configuration error; check product metadata.
-				Debug.Log("No products available for purchase!");
-				break;
+			Debug.Log ("No products available for purchase!");
+			break;
 		}
 	}
 
-	public void Awake()
+	public void Awake ()
 	{
-		var module = StandardPurchasingModule.Instance();
+		var module = StandardPurchasingModule.Instance ();
 
 		// The FakeStore supports: no-ui (always succeeding), basic ui (purchase pass/fail), and 
 		// developer ui (initialization, purchase, failure code setting). These correspond to 
 		// the FakeStoreUIMode Enum values passed into StandardPurchasingModule.useFakeStoreUIMode.
 		module.useFakeStoreUIMode = FakeStoreUIMode.StandardUser;
 
-		var builder = ConfigurationBuilder.Instance(module);
+		var builder = ConfigurationBuilder.Instance (module);
 		// This enables the Microsoft IAP simulator for local testing.
 		// You would remove this before building your release package.
-		builder.Configure<IMicrosoftConfiguration>().useMockBillingSystem = true;
+		builder.Configure<IMicrosoftConfiguration> ().useMockBillingSystem = true;
 
 		// Define our products.
 		// In this case our products have the same identifier across all the App stores,
@@ -197,33 +194,34 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 		// iOS stores.
 		// So on the Mac App store our products have different identifiers,
 		// and we tell Unity IAP this by using the IDs class.
-		builder.AddProduct("100.gold.coins", ProductType.Consumable, new IDs
-		{
-			{"100.gold.coins.mac", MacAppStore.Name},
-		});
-
-		builder.AddProduct("500.gold.coins", ProductType.Consumable, new IDs
-		{
-			{"500.gold.coins.mac", MacAppStore.Name},
-		});
-
-		builder.AddProduct("sword", ProductType.NonConsumable, new IDs
-		{
-			{"sword.mac", MacAppStore.Name}
-		});
-
-		builder.AddProduct("subscription", ProductType.Subscription, new IDs
-		{
-			{"subscription.mac", MacAppStore.Name}
-		});
+		builder.AddProduct ("pogoframe.001.diamonds", ProductType.Consumable);
+//		builder.AddProduct("100.gold.coins", ProductType.Consumable, new IDs
+//		{
+//			{"100.gold.coins.mac", MacAppStore.Name},
+//		});
+//
+//		builder.AddProduct("500.gold.coins", ProductType.Consumable, new IDs
+//		{
+//			{"500.gold.coins.mac", MacAppStore.Name},
+//		});
+//
+//		builder.AddProduct("sword", ProductType.NonConsumable, new IDs
+//		{
+//			{"sword.mac", MacAppStore.Name}
+//		});
+//
+//		builder.AddProduct("subscription", ProductType.Subscription, new IDs
+//		{
+//			{"subscription.mac", MacAppStore.Name}
+//		});
 
 		// Write Amazon's JSON description of our products to storage when using Amazon's local sandbox.
 		// This should be removed from a production build.
-		builder.Configure<IAmazonConfiguration>().WriteSandboxJSON(builder.products);
+		builder.Configure<IAmazonConfiguration> ().WriteSandboxJSON (builder.products);
 
 		// This enables simulated purchase success for Samsung IAP.
 		// You would remove this, or set to SamsungAppsMode.Production, before building your release package.
-		builder.Configure<ISamsungAppsConfiguration>().SetMode(SamsungAppsMode.AlwaysSucceed);
+		builder.Configure<ISamsungAppsConfiguration> ().SetMode (SamsungAppsMode.AlwaysSucceed);
 		// This records whether we are using Samsung IAP. Currently ISamsungAppsExtensions.RestoreTransactions
 		// displays a blocking Android Activity, so: 
 		// A) Unity IAP does not automatically restore purchases on Samsung Galaxy Apps
@@ -235,15 +233,15 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 		#endif
 
 		// Now we're ready to initialize Unity IAP.
-		UnityPurchasing.Initialize(this, builder);
+		UnityPurchasing.Initialize (this, builder);
 	}
 
 	/// <summary>
 	/// This will be called after a call to IAppleExtensions.RestoreTransactions().
 	/// </summary>
-	private void OnTransactionsRestored(bool success)
+	private void OnTransactionsRestored (bool success)
 	{
-		Debug.Log("Transactions restored.");
+		Debug.Log ("Transactions restored.");
 	}
 
 	/// <summary>
@@ -256,48 +254,46 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 	/// will fire.
 	/// </summary>
 	/// <param name="item">Item.</param>
-	private void OnDeferred(Product item)
+	private void OnDeferred (Product item)
 	{
-		Debug.Log("Purchase deferred: " + item.definition.id);
+		Debug.Log ("Purchase deferred: " + item.definition.id);
 	}
 
-	private void InitUI(IEnumerable<Product> items)
+	private void InitUI (IEnumerable<Product> items)
 	{
 		// Disable the UI while IAP is initializing
 		// See also UpdateInteractable()
-		m_InteractableSelectable = GetDropdown(); // References any one of the disabled components
+		m_InteractableSelectable = GetDropdown (); // References any one of the disabled components
 
 		// Show Restore button on Apple platforms
-		if (! (Application.platform == RuntimePlatform.IPhonePlayer || 
-			   Application.platform == RuntimePlatform.OSXPlayer ||
-			   m_IsSamsungAppsStoreSelected ) )
-		{
-			GetRestoreButton().gameObject.SetActive(false);
+		if (!(Application.platform == RuntimePlatform.IPhonePlayer ||
+		    Application.platform == RuntimePlatform.OSXPlayer ||
+		    m_IsSamsungAppsStoreSelected)) {
+			GetRestoreButton ().gameObject.SetActive (false);
 		}
 
-		foreach (var item in items)
-		{
+		foreach (var item in items) {
 			// Add initial pre-IAP-initialization content. Update later in OnInitialized.
-			var description = string.Format("{0} - {1}", item.definition.id, item.definition.type);
+			var description = string.Format ("{0} - {1}", item.definition.id, item.definition.type);
 
-			GetDropdown().options.Add(new Dropdown.OptionData(description));
+			GetDropdown ().options.Add (new Dropdown.OptionData (description));
 		}
 
 		// Ensure I render the selected list element
-		GetDropdown().RefreshShownValue();
+		GetDropdown ().RefreshShownValue ();
 
-		GetDropdown().onValueChanged.AddListener((int selectedItem) => {
-			Debug.Log("OnClickDropdown item " + selectedItem);
+		GetDropdown ().onValueChanged.AddListener ((int selectedItem) => {
+			Debug.Log ("OnClickDropdown item " + selectedItem);
 			m_SelectedItemIndex = selectedItem;
 		});
 
 		// Initialize my button event handling
-		GetBuyButton().onClick.AddListener(() => { 
+		GetBuyButton ().onClick.AddListener (() => { 
 			if (m_PurchaseInProgress == true) {
 				return;
 			}
 
-			m_Controller.InitiatePurchase(m_Controller.products.all[m_SelectedItemIndex]); 
+			m_Controller.InitiatePurchase (m_Controller.products.all [m_SelectedItemIndex]); 
 
 			// Don't need to draw our UI whilst a purchase is in progress.
 			// This is not a requirement for IAP Applications but makes the demo
@@ -305,101 +301,88 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 			m_PurchaseInProgress = true;
 		});
 
-		if (GetRestoreButton() != null)
-		{
-			GetRestoreButton().onClick.AddListener(() =>
-			{ 
-				if (m_IsSamsungAppsStoreSelected)
-				{
-					m_SamsungExtensions.RestoreTransactions(OnTransactionsRestored);
-				}
-				else
-				{
-					m_AppleExtensions.RestoreTransactions(OnTransactionsRestored);
+		if (GetRestoreButton () != null) {
+			GetRestoreButton ().onClick.AddListener (() => { 
+				if (m_IsSamsungAppsStoreSelected) {
+					m_SamsungExtensions.RestoreTransactions (OnTransactionsRestored);
+				} else {
+					m_AppleExtensions.RestoreTransactions (OnTransactionsRestored);
 				}
 			});
 		}
 	}
 
-	public void UpdateHistoryUI()
+	public void UpdateHistoryUI ()
 	{
-		if (m_Controller == null)
-		{
+		if (m_Controller == null) {
 			return;
 		}
 
 		var itemText = "Item\n\n";
 		var countText = "Purchased\n\n";
 
-		for (int t = 0; t < m_Controller.products.all.Length; t++)
-		{
+		for (int t = 0; t < m_Controller.products.all.Length; t++) {
 			var item = m_Controller.products.all [t];
 
 			// Collect history status report
 
 			itemText += "\n\n" + item.definition.id;
-			countText += "\n\n" + item.hasReceipt.ToString();
+			countText += "\n\n" + item.hasReceipt.ToString ();
 		}
 
 		// Show history
-		GetText(false).text = itemText;
-		GetText(true).text = countText;
+		GetText (false).text = itemText;
+		GetText (true).text = countText;
 	}
 
-	protected void UpdateInteractable()
+	protected void UpdateInteractable ()
 	{
-		if (m_InteractableSelectable == null)
-		{
+		if (m_InteractableSelectable == null) {
 			return;
 		}
 
 		bool interactable = m_Controller != null;
-		if (interactable != m_InteractableSelectable.interactable)
-		{
-			if (GetRestoreButton() != null)
-			{
-				GetRestoreButton().interactable = interactable;
+		if (interactable != m_InteractableSelectable.interactable) {
+			if (GetRestoreButton () != null) {
+				GetRestoreButton ().interactable = interactable;
 			}
-			GetBuyButton().interactable = interactable;
-			GetDropdown().interactable = interactable;
+			GetBuyButton ().interactable = interactable;
+			GetDropdown ().interactable = interactable;
 		}
 	}
 
-	public void Update()
+	public void Update ()
 	{
-		UpdateInteractable();
+		UpdateInteractable ();
 	}
 
-	private Dropdown GetDropdown()
+	private Dropdown GetDropdown ()
 	{
-		return GameObject.Find("Dropdown").GetComponent<Dropdown>();
+		return GameObject.Find ("Dropdown").GetComponent<Dropdown> ();
 	}
 
-	private Button GetBuyButton()
+	private Button GetBuyButton ()
 	{
-		return GameObject.Find("Buy").GetComponent<Button>();
+		return GameObject.Find ("Buy").GetComponent<Button> ();
 	}
 
 	/// <summary>
 	/// Gets the restore button when available
 	/// </summary>
 	/// <returns><c>null</c> or the restore button.</returns>
-	private Button GetRestoreButton()
+	private Button GetRestoreButton ()
 	{
-		GameObject restoreButtonGameObject = GameObject.Find("Restore");
-		if (restoreButtonGameObject != null)
-		{
-			return restoreButtonGameObject.GetComponent<Button>();
-		}
-		else
-		{
+		GameObject restoreButtonGameObject = GameObject.Find ("Restore");
+		if (restoreButtonGameObject != null) {
+			return restoreButtonGameObject.GetComponent<Button> ();
+		} else {
 			return null;
 		}
 	}
 
-	private Text GetText(bool right)
+	private Text GetText (bool right)
 	{
 		var which = right ? "TextR" : "TextL";
-		return GameObject.Find(which).GetComponent<Text>();
+		return GameObject.Find (which).GetComponent<Text> ();
 	}
 }
